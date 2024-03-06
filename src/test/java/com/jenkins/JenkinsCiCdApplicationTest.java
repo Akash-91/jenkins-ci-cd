0a90ee7d7221e6e542a53aded42ef9eaf0945b11
Akash-91 <akash.pyal22@gmail.com>
2024-03-06 15:13:04 +0100

    junit added

--- src/test/java/com/jenkins/JenkinsCiCdApplicationTest.java
+++ src/test/java/com/jenkins/JenkinsCiCdApplicationTest.java
@@ -1,12 +1,29 @@
 package com.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JenkinsCiCdApplicationTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void greetings() {
+    public void testGreetings() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/greetings/John Doe"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello John, welcome to your first jenkins demo project..!!"));
+    }
+
+    @Test
+    public void testGreetingsWithMultipleNames() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/greetings/John Doe Smith"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello John, welcome to your first jenkins demo project..!!"));
     }
 }
